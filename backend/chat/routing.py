0c5c990ecb165acb56a4eed784ddfb1bd5dacac2
abdoohossamm <abdoohossamm@outlook.com>
2022-06-16 02:24:02 +0200

    add websocket consumer routing

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
+from chat import consumers
 
-from . import consumers
-
+# that tells the client to listen to ws at consumers.py class
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
