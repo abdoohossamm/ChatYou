89d8e2eafd6402e77e265d1bbe0d8e7d0985e936
abdoohossamm <abdoohossamm@outlook.com>
2022-06-17 00:07:31 +0200

    send user in context to the template

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'chat/index.html')
+@login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'username': request.user.username
     })
