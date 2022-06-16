82ec3fab71d92a84738eda11bd5df698e2ba67fd
abdoohossamm <abdoohossamm@outlook.com>
2022-06-17 00:06:05 +0200

    make model Message

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Message(models.Model):
+    user = models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
+    content = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username[:10]}: {self.content[:10]}"
+    def last_30_msgs():
+        return Message.objects.order_by("-time").all()[:30]
