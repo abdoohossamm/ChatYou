6aee2aab8007b4a9ef93f95d182bbf09943ad41e
abdoohossamm <abdoohossamm@outlook.com>
2022-12-27 10:41:57 +0200

    add ACCOUNT_ACTIVATION_DAYS to settings to fix registration bug

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+ACCOUNT_ACTIVATION_DAYS = 7
 
 LOGOUT_REDIRECT_URL = 'chat:index'
 LOGIN_REDIRECT_URL = 'chat:index'
